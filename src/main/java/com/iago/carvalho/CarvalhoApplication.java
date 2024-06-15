eba893c9329d102123b190c122366bee7202b87d
iagocarvalho <71970033+iagocarvalho07@users.noreply.github.com>
2024-06-15 18:24:01 -0300

    Initial commit

---
+++ src/main/java/com/iago/carvalho/CarvalhoApplication.java
@@ -0,0 +1,13 @@
+package com.iago.carvalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarvalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarvalhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iago/carvalho/CarvalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iago.carvalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarvalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
